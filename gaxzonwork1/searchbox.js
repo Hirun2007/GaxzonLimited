68eaf557782e4510e1a58a900e35402072fc3cee
Hirun2007 <145740879+Hirun2007@users.noreply.github.com>
2023-09-29 23:47:35 +0530

    Delete gaxzonwork1 directory

--- gaxzonwork1/searchbox.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("search-button").addEventListener("click", function () {
-  var searchTerm = document.getElementById("search-input").value;
-  alert("You searched for: " + searchTerm);
-});
